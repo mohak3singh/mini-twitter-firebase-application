55ef33fc0b7d51a4b99bc81f2cb7c932d5084891
Mohak Singh <mohak03singh@gmail.com>
2021-08-06 15:47:08 +0530

    third-commit

--- App.js
+++ App.js
@@ -5,22 +5,21 @@ import "./index.css";
 function App() {
   let [posts, setPosts] = useState([]);
 
-  useEffect(() => {
+  useEffect(() => { 
     let f = async () => {
-      await firestore.collection("posts").onSnapshot((querySnapshot)=>{
-
+      await firestore.collection("posts").onSnapshot((querySnapshot) => {
         let tempArr = [];
-        
+
         querySnapshot.forEach((doc) => {
           tempArr.push({
             id: doc.id,
             data: doc.data(),
           });
         });
-        
+
         setPosts(tempArr);
       });
-      };
+    };
 
     f();
   }, []);
@@ -29,18 +28,25 @@ function App() {
     <div>
       <ul>
         {posts.map((el) => (
-          <li className="all-list" key={el.id}>{el.data.Body}</li>
+          <li className="all-list" key={el.id}>
+            {el.data.Body}
+          </li>
         ))}
       </ul>
 
-      <input className="input-box" type="text" placeholder="Type here" onKeyDown={(e)=>{
-        if(e.code === "Enter"){
-          // jobhi likha hua hai vo mujhe firebase me dalna hai
-          let post = e.currentTarget.value
-          firestore.collection("posts").add({Body:post});
-          e.currentTarget.value = "";
-        }
-      }}/>
+      <input
+        className="input-box"
+        type="text"
+        placeholder="Type here"
+        onKeyDown={(e) => {
+          if (e.code === "Enter") {
+            // jobhi likha hua hai vo mujhe firebase me dalna hai
+            let post = e.currentTarget.value;
+            firestore.collection("posts").add({ Body: post });
+            e.currentTarget.value = "";
+          }
+        }}
+      />
     </div>
   );
 }
--- firebase.js
+++ firebase.js
@@ -2,16 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCARTUhmbdwj-NJe6l9_qO72_0gy5tN6bU",
-    authDomain: "twitter-mini-eeff4.firebaseapp.com",
-    projectId: "twitter-mini-eeff4",
-    storageBucket: "twitter-mini-eeff4.appspot.com",
-    messagingSenderId: "437183924522",
-    appId: "1:437183924522:web:440439463a882ddaa87653"
-  };
+  apiKey: "AIzaSyCARTUhmbdwj-NJe6l9_qO72_0gy5tN6bU",
+  authDomain: "twitter-mini-eeff4.firebaseapp.com",
+  projectId: "twitter-mini-eeff4",
+  storageBucket: "twitter-mini-eeff4.appspot.com",
+  messagingSenderId: "437183924522",
+  appId: "1:437183924522:web:440439463a882ddaa87653",
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  export const firestore = firebase.firestore();
+export const firestore = firebase.firestore();
 
-  export default firebase;
+export default firebase;
